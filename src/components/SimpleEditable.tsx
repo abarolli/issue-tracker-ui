37093b0edafc30ab9c47d3ecab1ada9500908286
abarolli <obarolli@gmail.com>
2025-03-23 15:56:05 -0700

    Add SimpleEditable component for enhanced editable text functionality

---
+++ src/components/SimpleEditable.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import { Editable, IconButton } from "@chakra-ui/react";
+import { LuCheck, LuX } from "react-icons/lu";
+import { UseFormRegisterReturn } from "react-hook-form";
+
+interface SimpleEditableProps {
+  content: string;
+  fontSize?: string;
+  register?: UseFormRegisterReturn<string>;
+}
+
+function SimpleEditable({ content, fontSize, register }: SimpleEditableProps) {
+  return (
+    <Editable.Root defaultValue={content}>
+      <Editable.Preview w="100%" fontSize={fontSize || "lg"} />
+      <Editable.Input {...register} w="100%" fontSize={fontSize || "lg"} />
+      <Editable.Control>
+        <Editable.CancelTrigger asChild>
+          <IconButton variant="outline" size="sm">
+            <LuX />
+          </IconButton>
+        </Editable.CancelTrigger>
+        <Editable.SubmitTrigger asChild>
+          <IconButton variant="outline" size="sm">
+            <LuCheck />
+          </IconButton>
+        </Editable.SubmitTrigger>
+      </Editable.Control>
+    </Editable.Root>
+  );
+}
+
+export default SimpleEditable;
