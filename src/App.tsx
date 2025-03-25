a66401b11b7e818ebef7b1edec1e88f7e9030901
abarolli <obarolli@gmail.com>
2025-03-25 14:17:57 -0700

    Refactor App component to implement routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Box } from "@chakra-ui/react";
+import LoginForm from "./components/LoginForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CreateIssueForm } from "./components/IssueDisplay";
+import ROUTES from "./configs/routes";
+import IssueRetriever from "./components/IssueRetriever";
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+interface AppProps {
+  className?: string;
 }
 
-export default App
+function App({ className }: AppProps) {
+  return (
+    <Box className={className}>
+      <Router>
+        <Routes>
+          <Route path={ROUTES.LOGIN} element={<LoginForm />} />
+          <Route path={ROUTES.CREATE_ISSUE} element={<CreateIssueForm />} />
+          <Route path="/issues/:id" element={<IssueRetriever />} />
+        </Routes>
+      </Router>
+    </Box>
+  );
+}
+
+export default App;
