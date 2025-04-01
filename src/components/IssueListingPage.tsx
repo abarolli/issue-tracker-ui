17ce3be090d2131fe7e1e85e45207e5e46fe134e
abarolli <obarolli@gmail.com>
2025-03-31 17:02:45 -0700

    Add IssueListingPage component to render IssueListing

---
+++ src/components/IssueListingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import IssueListing from "./IssueListing";
+import { Flex } from "@chakra-ui/react";
+
+function IssueListingPage() {
+  return (
+    <div>
+      <IssueListing />
+    </div>
+  );
+}
+
+export default IssueListingPage;
