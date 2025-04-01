c88effb241056fdb3e8efca62004a175ae5ae72a
abarolli <obarolli@gmail.com>
2025-04-01 15:48:22 -0700

    Refactor IssueRetriever to accept id as a prop and improve error handling for issue retrieval

--- src/components/IssueRetriever.tsx
+++ src/components/IssueRetriever.tsx
@@ -7,15 +7,23 @@ import IssueService from "../services/issue-service";
 import { Issue } from "./util-types/Issue";
 import { HttpStatusCode } from "axios";
 
-function IssueRetriever() {
-  const id = Number.parseInt(useParams().id!);
+interface IssueRetrieverProps {
+  id?: number;
+}
+
+function IssueRetriever({ id }: IssueRetrieverProps) {
+  let paramId: any = useParams().id ?? id;
+  if (paramId === null || paramId === undefined)
+    throw new Error("Could not determine id for Issue retrieval");
+
+  paramId = typeof paramId === "string" ? Number.parseInt(paramId) : paramId;
   const [issue, setIssue] = useState<Issue | null>(null);
   const navigate = useNavigate();
   const issueService = new IssueService();
 
   useEffect(() => {
     issueService
-      .getIssue(id)
+      .getIssue(paramId)
       .then((data) => {
         setIssue(data);
       })
@@ -26,11 +34,11 @@ function IssueRetriever() {
         )
           navigate(ROUTES.LOGIN);
       });
-  }, []);
+  }, [paramId]);
 
   const updateIssue: SubmitHandler<FieldValues> = (data: FieldValues) => {
     issueService
-      .updateIssue(id, data)
+      .updateIssue(paramId, data)
       .then((data) => console.log(data))
       .catch(({ status }) => {
         if (
